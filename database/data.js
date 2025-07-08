88fc014e13a39651f7fd0b794f41e447244c915e
wilo <sudowilo@gmail.com>
2025-07-08 13:28:27 +0330

    getData function for getting saved data

--- database/data.js
+++ database/data.js
@@ -1,11 +1,20 @@
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
 const path = require('node:path');
 
 const dataPath = path.join(__dirname, 'data.json');
-fs.readFile(dataPath, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
+
+async function getData() {
+    try {
+        const json = await fs.readFile(dataPath, 'utf-8');
+        const obj = JSON.parse(json);
+        return obj;
+    } catch (error) {
+        if (error.code == 'ENOENT') {
+            await fs.writeFile(dataPath, '{"days":{}}');
+            getData();
+        }
+        console.log('catch you', error.code);
     }
-    console.log(data);
-});
+}
+
+getData();
